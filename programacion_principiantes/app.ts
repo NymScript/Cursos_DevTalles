bbb60bf6adbb235074c37056dd748436c983a584
NymScript <niyimorera28@gmail.com>
2024-09-11 17:46:09 -0500

    Archivo funciones para liberar app.ts

--- programacion_principiantes/app.ts
+++ programacion_principiantes/app.ts
@@ -1,21 +1,4 @@
-let NumbersOfCats = 1;
-
-//* Funciones
-
-function increaseLineNumber() {
-
-    NumbersOfCats+=3;
-
-}
-
-increaseLineNumber();
-console.log('Cat', NumbersOfCats);
-
-increaseLineNumber();
-console.log('Cat', NumbersOfCats);
-
-increaseLineNumber();
-console.log('Cat' , NumbersOfCats);
+console.log("Hi")
 
 
 
---
+++ programacion_principiantes/exercises/Funciones.js
@@ -0,0 +1,21 @@
+let NumbersOfCats = 1;
+
+//* Funciones
+
+function increaseLineNumber() {
+
+    NumbersOfCats+=3;
+
+}
+
+increaseLineNumber();
+console.log('Cat', NumbersOfCats);
+
+increaseLineNumber();
+console.log('Cat', NumbersOfCats);
+
+increaseLineNumber();
+console.log('Cat' , NumbersOfCats);
+
+
+
