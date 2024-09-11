a95851ac4e3057ff05abc679ebf29ad51cd18b5d
NymScript <niyimorera28@gmail.com>
2024-09-11 17:23:23 -0500

    Nuevo nombre, carpeta con ejercicios y ejercicio en app.ts

---
+++ programacion_principiantes/app.ts
@@ -0,0 +1,13 @@
+let NumbersOfCats = 1;
+
+NumbersOfCats++;
+console.log('Hola', NumbersOfCats);
+
+NumbersOfCats++;
+console.log('Hola', NumbersOfCats);
+
+NumbersOfCats++;
+console.log('Hola' , NumbersOfCats);
+
+
+
