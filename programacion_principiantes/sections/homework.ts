c20592c3c37e207a9ef567c33ab167087c13ca2a
NymScript <niyimorera28@gmail.com>
2024-09-20 17:30:21 -0500

    Tarea seccion 3 para 4

---
+++ programacion_principiantes/sections/homework.ts
@@ -0,0 +1,16 @@
+//* Multiplicación
+
+let base = 10;
+let limit = 50;
+
+// for (let i = 1; i <= limit; i++) {
+//     console.log(base, ' x ', i, ' = ', base * i);
+// }
+
+// do While
+
+let i = 1;
+do {
+    console.log( base, ' x ', i, ' = ', base * i);
+    i++;
+} while (i <= limit);
